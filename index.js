931108e8de206b5557b816c3fb036cc030f1866c
Hollister009 <reactive.andrew@yahoo.com.ua>
2022-01-21 10:45:49 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// TODO: read the package.json file
+
+// TODO: extract all the dependencies
+
+// TODO: update all the dependencies
+
+// TODO: commit updates
+
+// TODO: push changes to remote repo
+
+// TODO: open the PR in remote repo
